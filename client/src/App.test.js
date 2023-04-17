3e69c55cdbc46bf124f6599d12be01e67c6106e4
NzauM <mercywaenu16@gmail.com>
2023-04-17 11:08:45 +0300

    add react testing workflows

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Newuser from './Newuser';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('Render User Registration Form',() => {
+  render(<Newuser/>);
+  const namefield = screen.getAllByLabelText("Name");
+  const hobbyfield = screen.getAllByLabelText("Hobby");
+  const picfield = screen.getAllByLabelText("Enter profile Picture")
+  expect(namefield, hobbyfield, picfield).toBeInTheDocument();
+})
