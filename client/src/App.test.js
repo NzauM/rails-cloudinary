41cf6b41b9028e6a475da799331536639b554b8f
NzauM <mercywaenu16@gmail.com>
2023-04-18 11:52:04 +0300

    add tests for customer registration form

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render,fireEvent, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import Newuser from './Newuser';
 
@@ -8,10 +8,33 @@ import Newuser from './Newuser';
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-test('Render User Registration Form',() => {
+test('Render User Registration Form', () => {
   render(<Newuser/>);
-  const namefield = screen.getAllByLabelText("Name");
+  const namefield = screen.getByLabelText("Name");
   const hobbyfield = screen.getAllByLabelText("Hobby");
   const picfield = screen.getAllByLabelText("Enter profile Picture")
-  expect(namefield, hobbyfield, picfield).toBeInTheDocument();
+  expect(namefield).toBeInTheDocument();
+});
+
+test('addCustomer function submits form data', async()=>{
+  const mockFetch = jest.fn(()=>Promise.resolve({ok: true}));
+  globalThis.fetch = mockFetch
+
+  render (<Newuser/>);
+
+  // Find the form fields and fill them out
+  const nameInput = screen.getByLabelText('Name');
+  const hobbyInput = screen.getByLabelText('Hobby');
+  const imageInput = screen.getByLabelText('Enter profile Picture');
+  fireEvent.change(nameInput, { target: { value: 'John Doe' } });
+  fireEvent.change(hobbyInput, { target: { value: 'Soccer' } });
+  fireEvent.change(imageInput, { target: { files: [new File([''], 'test.png', { type: 'image/png' })] } });
+
+
+  fireEvent.submit(screen.getByTestId('customer-form'));
+  await waitFor(() => expect(mockFetch).toHaveBeenCalledTimes(1));
+  
+  expect(mockFetch.mock.calls[0][1].body.get('name')).toEqual('John Doe');
+  expect(mockFetch.mock.calls[0][1].body.get('hobby')).toEqual('Soccer');
+  expect(mockFetch.mock.calls[0][1].body.get('customer_image').name).toEqual('customer_image');
 })
